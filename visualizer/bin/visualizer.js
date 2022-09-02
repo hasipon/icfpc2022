0214721c47b6892298a8a87d201416e38dc566ba
shohei909 <909mmsphere@gmail.com>
2022-09-02 22:41:12 +0900

    draw input

--- visualizer/bin/visualizer.js
+++ visualizer/bin/visualizer.js
@@ -1,7 +1,16 @@
 (function ($global) { "use strict";
 var Main = function() { };
 Main.main = function() {
-	Main.canvas = window.document.getElementById("main");
+	Main.mainCanvas = window.document.getElementById("output");
+	Main.mainPixi = new PIXI.Application({ view : Main.mainCanvas, transparent : true, width : Main.mainCanvas.width, height : Main.mainCanvas.height, autoResize : true});
+	Main.mainPixi.stage.interactive = true;
+	Main.mainPixi.stage.addChild(Main.outputLayer = new PIXI.Graphics());
+	Main.mainPixi.stage.addChild(Main.problemLayer = new PIXI.Sprite());
+	Main.mainPixi.stage.addChild(Main.borderLayer = new PIXI.Graphics());
+	Main.outputLayer.x = Main.outputLayer.y = 20;
+	Main.problemLayer.x = Main.problemLayer.y = 20;
+	Main.borderLayer.x = Main.borderLayer.y = 20;
+	Main.problemLayer.alpha = 0.3;
 	Main.problemInput = window.document.getElementById("problem");
 	Main.problemInput.onchange = Main.onProblemChanged;
 	Main.problemInput.oninput = Main.onProblemChanged;
@@ -11,8 +20,14 @@ Main.main = function() {
 };
 Main.onProblemChanged = function() {
 	Main.imageElement.src = "../problems/" + Main.problemInput.value + ".png";
+	if(Main.problemLayer.texture != null) {
+		Main.problemLayer.texture.destroy();
+	}
+	Main.problemLayer.texture = PIXI.Texture.fromImage("../problems/" + Main.problemInput.value + ".png");
 };
 Main.onImageLoad = function() {
+};
+Main.write = function() {
 };
 var haxe_iterators_ArrayIterator = function(array) {
 	this.current = 0;
--- visualizer/src/Main.hx
+++ visualizer/src/Main.hx
@@ -1,22 +1,45 @@
 package;
 import js.Browser;
-import js.Lib;
 import js.html.CanvasElement;
-import js.html.Element;
-import js.html.Image;
 import js.html.ImageElement;
 import js.html.InputElement;
+import pixi.core.Application;
+import pixi.core.graphics.Graphics;
+import pixi.core.sprites.Sprite;
+import pixi.core.textures.Texture;
 
 class Main 
 {
-	public static var problemInput:InputElement;
-	public static var imageElement:ImageElement;
-	public static var canvas:CanvasElement;
-	public static var image:Image;
+	static var input       :InputElement;
+	static var problemInput:InputElement;
+	static var imageElement:ImageElement;
+	
+	static var mainCanvas:CanvasElement;
+	static var mainPixi:Application;
+	static var borderLayer :Graphics;
+	static var outputLayer :Graphics;
+	static var problemLayer:Sprite;
+	static var state:State;
 	
 	static function main()
 	{
-		canvas = cast Browser.document.getElementById("main");
+		mainCanvas = cast Browser.document.getElementById("output");
+	
+		mainPixi = new Application({
+			view  :mainCanvas,
+			transparent: true,
+			width: mainCanvas.width,
+			height: mainCanvas.height,
+			autoResize: true,
+		});
+		mainPixi.stage.interactive = true;
+		mainPixi.stage.addChild(outputLayer  = new Graphics());
+		mainPixi.stage.addChild(problemLayer = new Sprite());
+		mainPixi.stage.addChild(borderLayer  = new Graphics());
+		outputLayer .x = outputLayer .y = 20;
+		problemLayer.x = problemLayer.y = 20;
+		borderLayer .x = borderLayer .y = 20;
+		problemLayer.alpha = 0.3;
 		
 		problemInput = cast Browser.document.getElementById("problem");
 		problemInput.onchange = onProblemChanged;
@@ -31,9 +54,19 @@ class Main
 	static function onProblemChanged():Void
 	{
 		imageElement.src = "../problems/" + problemInput.value + ".png";
+		
+		if (problemLayer.texture != null)
+		{
+			problemLayer.texture.destroy();
+		}
+		problemLayer.texture = Texture.fromImage("../problems/" + problemInput.value + ".png");
 	}
 	
 	static function onImageLoad():Void 
+	{
+	}
+	
+	static function write():Void
 	{
 	}
 }
