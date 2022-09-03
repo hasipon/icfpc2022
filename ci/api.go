9aa362d8e74e8333fe62a0494c33d530891b5dd1
Shingo INADA <mecha.is06@gmail.com>
2022-09-03 17:22:45 +0900

    update ci

--- ci/api.go
+++ ci/api.go
@@ -13,22 +13,21 @@ import (
 var client = &http.Client{}
 
 func CallSubmitApi(problemID int, name string, isl string) error {
+	requestBody := new(bytes.Buffer)
+	writer := multipart.NewWriter(requestBody)
+	part, err := writer.CreateFormFile("file", name)
+	_, err = part.Write([]byte(isl))
+	if err != nil {
+		return err
+	}
+
 	url := fmt.Sprintf("https://robovinci.xyz/api/submissions/%d/create", problemID)
-	req, err := http.NewRequest("POST", url, nil)
+	req, err := http.NewRequest("POST", url, requestBody)
 	if err != nil {
 		return err
 	}
 
 	req.Header.Set("Authorization", "Bearer "+os.Getenv("API_TOKEN"))
-	{
-		body := new(bytes.Buffer)
-		writer := multipart.NewWriter(body)
-		part, err := writer.CreateFormFile("file", name)
-		_, err = part.Write([]byte(isl))
-		if err != nil {
-			return err
-		}
-	}
 
 	resp, err := client.Do(req)
 	if err != nil {
