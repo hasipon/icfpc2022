924f146242d36365a33d7ca3f11847678a032662
Shingo INADA <mecha.is06@gmail.com>
2022-09-03 20:30:39 +0900

    visualizer link

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,6 +6,7 @@ import shutil
 import json
 from collections import defaultdict
 
+import flask
 from PIL import Image
 from typing import *
 from flask import Flask, request, render_template
@@ -14,6 +15,7 @@ from sqlalchemy import Column, Integer, String, Float, DateTime
 
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
+visualizer_url = "http://34.85.55.117/repo/visualizer"
 static_path = pathlib.Path(__file__).resolve().parent / 'static'
 repo_path = pathlib.Path(__file__).resolve().parent.parent
 problems_path = repo_path / "problems"
@@ -94,6 +96,12 @@ def index():
         result_by_api=result_by_api,
         problems_dict=problems_dict
     )
+
+
+@app.route('/vis/<solution>')
+def get_vis(solution: str):
+    problem_id, isl = engine.execute("SELECT problem_id, isl FROM solution WHERE id=%s", (solution,)).fetchone()
+    return flask.redirect(visualizer_url + f"/#{problem_id};{isl}")
 
 
 @app.route('/filter')
