6b3059b9963adcacaed197d1cd7c7584c2ea521e
Tetsuya Shiota <tetsuya.shiota.1231@gmail.com>
2022-09-05 02:45:01 +0900

    re-run shiota5

--- ai/shiota5/run.sh
+++ ai/shiota5/run.sh
@@ -9,3 +9,12 @@ for i in {1..25} ; do
   cd -
   BLOCK_ID_INFO=info MERGE_ISL=../../solutions.best/${PROBLEM_ID}.isl ./a.out > ../../solutions/${PROBLEM_ID}-shiota5-`date +%s`.isl
 done
+
+for i in {36..40} ; do
+  export PROBLEM_ID=$i
+  PROBLEM_ID=${PROBLEM_ID}
+  cd ../../eval-v2
+  BLOCK_ID_INFO=../ai/shiota5/info ISL_FILE=../solutions.best/${PROBLEM_ID}.isl npx ts-node index.ts
+  cd -
+  BLOCK_ID_INFO=info MERGE_ISL=../../solutions.best/${PROBLEM_ID}.isl ./a.out > ../../solutions/${PROBLEM_ID}-shiota5-`date +%s`.isl
+done
