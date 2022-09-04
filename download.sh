7a9ec43061e6e552731907f7017d6cd02e6d285b
DiamondPrincess <ubuntu@instance-7.asia-northeast1-b.c.diamondprincess-211611.internal>
2022-09-04 15:21:05 +0000

    add problems

--- download.sh
+++ download.sh
@@ -4,10 +4,9 @@ for i in $(seq 36 40); do \
     curl https://cdn.robovinci.xyz/imageframes/${i}.png > problems/${i}.png; \
     convert problems/${i}.png problems.pam/${i}.pam; \
 
-    curl https://cdn.robovinci.xyz/imageframes/${i}.initial.png > problems/${i}.initial.png; \
     curl https://cdn.robovinci.xyz/imageframes/${i}.initial.json > problems/${i}.initial.json; \
 
-    curl https://cdn.robovinci.xyz/imageframes/${i}.source.png > problems/${i}.source.png; \
-    curl https://cdn.robovinci.xyz/imageframes/${i}.source.json > problems/${i}.source.json; \
+    curl https://cdn.robovinci.xyz/sourcepngs/${i}.source.png > problems/${i}.source.png; \
+    curl https://cdn.robovinci.xyz/sourcepngs/${i}.source.json > problems.json/${i}.source.json; \
     convert problems/${i}.png problems.pam/${i}.source.pam; \
 done
