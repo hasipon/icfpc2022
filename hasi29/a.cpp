9bfd75cdfb953f381b525a4d9613874b5d163c12
hasi <hasipon@users.noreply.github.com>
2022-09-05 10:22:24 +0000

    hasi29

--- hasi29/a.cpp
+++ hasi29/a.cpp
@@ -179,7 +179,7 @@ struct Data2 {
 void solve() {
 	string blockId = "0";
 	int globalCounter = 0;
-	vector<int> ipos {400,350,335,298,277,255,233,210,173,159,111,88,63,30,0};
+	vector<int> ipos {400,375,350,335,298,277,255,233,210,173,159,111,88,63,30,0};
 	for (int p = 1; p < (int)ipos.size(); ++ p) {
 		if (p > 1) {
 			cout << "cut ["<<blockId<<"] [y] ["<<ipos[p-1]<<"]" << endl;
